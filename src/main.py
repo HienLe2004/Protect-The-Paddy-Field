2b31b46ffbcfc364a50432e5cffdb1202d11699d
HienLe20044 <hien.lengoc2004@hcmut.edu.vn>
2025-01-16 13:30:33 +0700

    Update game state manager

--- src/main.py
+++ src/main.py
@@ -4,18 +4,13 @@ from rice_plant import RicePlant
 from score import Score
 from game_clock import Game_Clock
 
-class Game:
-    def __init__(self):
+class Game_Play:
+    def __init__(self,screen,game_state_manager):
         # setup
-        pygame.init()
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        pygame.display.set_caption('Protect The Paddy Field')
-        pygame.display.set_icon(pygame.image.load('images/rice.png'))
-        self.clock = pygame.time.Clock() 
-        self.running = True
-        self.background_image = pygame.image.load('images/background.jpg')
-        self.background_image = pygame.transform.scale(self.background_image, self.screen.get_size())
+        self.screen = screen
+        self.game_state_manager = game_state_manager
         pygame.mouse.set_visible(False)
+        self.running = True
         # sounds
         self.vole_sounds = [pygame.mixer.Sound(f'sounds/vole{i + 1}.wav') for i in range(3)]
         self.shooting_sounds = pygame.mixer.Sound('sounds/shoot.wav')
@@ -37,14 +32,13 @@ class Game:
         self.slingshot = Slingshot(self.player_sprites)
         self.score = Score()
         self.game_clock = Game_Clock()
-        self.game_state = Game_State.PLAYING
 
-    def run(self):
+    def run(self, dt):
         while self.running:
-            dt = self.clock.tick(60) / 1000
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                    self.game_state_manager.set_state('quit')
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_SPACE and not self.game_clock.start:
                         self.game_clock.start_clock()
@@ -76,18 +70,55 @@ class Game:
             self.game_clock.draw(self.screen)
             self.player_sprites.draw(self.screen)
             pygame.display.update()
-        pygame.quit()
 
     def play_vole_dead_sound(self):
         pygame.time.delay(100)
         index = random.randint(0, len(self.vole_sounds) - 1)
         self.vole_sounds[index].play()
 
-class Game_State(enumerate):
-    MENU = 0
-    STARTING = 1
-    PLAYING = 2
-    GAME_OVER = 3
+class Main_Menu():
+    def __init__(self, screen, game_state_manager):
+        self.screen = screen
+        self.game_state_manager = game_state_manager
+    def run(self):
+        pass
+
+class Game():
+    def __init__(self):
+        self.game_state_manager = Game_State_Manager('gameplay')
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption('Protect The Paddy Field')
+        pygame.display.set_icon(pygame.image.load('images/rice.png'))
+        self.clock = pygame.time.Clock() 
+        self.running = True
+        self.background_image = pygame.image.load('images/background.jpg')
+        self.background_image = pygame.transform.scale(self.background_image, self.screen.get_size())
+        self.game_play = Game_Play(self.screen, self.game_state_manager)
+        self.main_menu = Main_Menu(self.screen, self.game_state_manager)
+        self.states = {'gameplay':self.game_play, 'main_menu':self.main_menu}
+
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            dt = self.clock.tick(60) / 1000
+
+            if (self.game_state_manager.get_state() is 'quit'):
+                self.running = False
+            else:
+                self.states[self.game_state_manager.get_state()].run(dt)
+
+        pygame.quit()
+
+class Game_State_Manager():
+    def __init__(self, current_state):
+        self.current_state = current_state
+    def set_state(self, current_state):
+        self.current_state = current_state
+    def get_state(self):
+        return self.current_state
 
 if __name__ == '__main__':
     game = Game()
